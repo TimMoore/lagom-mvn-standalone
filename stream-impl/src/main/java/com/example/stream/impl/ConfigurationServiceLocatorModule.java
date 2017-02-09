3b777a322ea3442976e0b4e81977de293f3d4168
Tim Moore <tim.moore@lightbend.com>
2017-02-09 17:05:54 +1100

    Add static service locators and port bindings

---
+++ stream-impl/src/main/java/com/example/stream/impl/ConfigurationServiceLocatorModule.java
@@ -0,0 +1,25 @@
+package com.example.stream.impl;
+
+import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.api.ConfigurationServiceLocator;
+import com.lightbend.lagom.javadsl.api.ServiceLocator;
+import play.Configuration;
+import play.Environment;
+
+public class ConfigurationServiceLocatorModule extends AbstractModule {
+
+    private final Environment environment;
+    private final Configuration configuration;
+
+    public ConfigurationServiceLocatorModule(Environment environment, Configuration configuration) {
+        this.environment = environment;
+        this.configuration = configuration;
+    }
+
+    @Override
+    protected void configure() {
+        if (environment.isProd()) {
+            bind(ServiceLocator.class).to(ConfigurationServiceLocator.class);
+        }
+    }
+}
